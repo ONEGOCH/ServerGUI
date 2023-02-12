79d806b197038182a0073efd1c28436b1acf9eb4
Биленко Георгий Андреевич <sprinkfild@mail.ru>
2023-02-12 23:03:21 +0500

    UpLoad1

--- Form.cs
+++ Form.cs
@@ -19,65 +19,42 @@ namespace ServerGUI
 
         private void StartBtn_Click(object sender, EventArgs e)
         {
-            var bit = Environment.Is64BitOperatingSystem ? "64" : "";
+            try
+            {
+                var bit = Environment.Is64BitOperatingSystem ? "64" : "";
 
-            var process = new Process();
-            var startInfo = new ProcessStartInfo
+                var process = new Process();
+                var startInfo = new ProcessStartInfo
+                {
+                    WindowStyle = ProcessWindowStyle.Hidden,
+                    FileName = "cmd.exe",
+                    Arguments =
+                        $@"/C C:\Windows\Microsoft.NET\Framework{bit}\v4.0.30319\InstallUtil.exe {Directory.GetCurrentDirectory()}\ServiceServer.exe"
+                };
+                process.StartInfo = startInfo;
+                var config = new ServiceConfig
+                {
+                    port = txbPort.Text,
+                    clientsIp = new List<DataGridViewRow>(dgv.Rows.Cast<DataGridViewRow>())
+                        .Select(x => x.Cells[0].Value.ToString())
+                        .ToList(),
+                    licFile = txbKey.Text,
+                    logFile = txbLog.Text
+                };
+                var configString = JsonSerializer.Serialize(config);
+                using (var file = File.Open("serverConfig.json", FileMode.OpenOrCreate))
+                    foreach (var symbol in configString)
+                        file.WriteByte(Convert.ToByte(symbol));
+                if (config.clientsIp.Count == 0)
+                    throw new Exception("Укажите хотя бы одного клиента!");
+
+                process.Start();
+
+            }
+            catch (Exception ex)
             {
-                WindowStyle = ProcessWindowStyle.Hidden,
-                FileName = "cmd.exe",
-                Arguments =
-                    $@"/C C:\Windows\Microsoft.NET\Framework{bit}\v4.0.30319\InstallUtil.exe {Directory.GetCurrentDirectory()}\ServiceServer.exe"
-            };
-            process.StartInfo = startInfo;
-            var config = new ServiceConfig
-            {
-                port = txbPort.Text,
-                clientsIp = new List<DataGridViewRow>(dgv.Rows.Cast<DataGridViewRow>())
-                    .Select(x => x.Cells[0].Value.ToString())
-                    .ToList(),
-                licFile = txbKey.Text,
-                logFile = txbLog.Text
-            };
-            var configString = JsonSerializer.Serialize(config);
-            using (var file = File.Open("serverConfig.json",FileMode.OpenOrCreate))
-                foreach (var symbol in configString)
-                    file.WriteByte(Convert.ToByte(symbol));
-            if (config.clientsIp.Count == 0)
-                throw new Exception("Укажите хотя бы одного клиента!");
-            
-            process.Start();
-            return;
-            // try
-            // {
-            //     var myProcess = new Process();
-            //
-            //     myProcess.StartInfo.FileName = txbServer.Text;
-            //
-            //     if (chbRunOnBackGround.Checked)
-            //         myProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-            //
-            //     var ipClientsDgvrs = new List<DataGridViewRow>(dgv.Rows.Cast<DataGridViewRow>());
-            //
-            //     
-            //
-            //     var ipClientsList = ipClientsDgvrs.Select(x => x.Cells[0].Value.ToString()).ToList();
-            //     var ipClients = string.Join(";", ipClientsList);
-            //
-            //     var keyStr = $"\"{txbKey.Text}\"";
-            //     var portStr = $"\"{txbPort.Text}\"";
-            //     var ipClientsStr = $"\"{ipClients}\"";
-            //     var logStr = $"\"{txbLog.Text}\"";
-            //
-            //     var argStr = string.Join(" ", new string[] { keyStr, portStr, ipClientsStr, logStr });
-            //
-            //     myProcess.StartInfo.Arguments = argStr;
-            //     myProcess.Start();
-            // }
-            // catch (Exception ex)
-            // {
-            //     MessageBox.Show(ex.Message);
-            // }
+                MessageBox.Show(ex.Message);
+            }           
         }
 
         private void InfoBtb_Click(object sender, EventArgs e)
